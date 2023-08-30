fb5d3011ac2801a41af032b7c30a2cde337054e1
BrianaRobinson <142817857+BrianaRobinson@users.noreply.github.com>
2023-08-29 21:19:16 -0500

    Create 01.Project.py

---
+++ 01.Project.py
@@ -0,0 +1,8 @@
+1 year = 365 days
+1 year = 52 weeks = 365 days
+1 week = 7 days
+
+def time_in_days(day, hours, minutes):
+    return year * 365 + weeks * 365 + week * 7
+
+print(time_in_days(0,48,4))
