631b7df5fc5fd4c4ed1104782068cf4b78f3d37c
BrianaRobinson <142817857+BrianaRobinson@users.noreply.github.com>
2023-08-29 21:40:06 -0500

    Update 01.Project.py

--- 01.Project.py
+++ 01.Project.py
@@ -1,8 +1,6 @@
-1 year = 365 days
-1 year = 52 weeks = 365 days
-1 week = 7 days
+years = int(input("Input years :"))*365
+weeks = int(input("Input weeks :"))*7
+days = days
 
-def time_in_days(day, hours, minutes):
-    return year * 365 + weeks * 365 + week * 7
-
-print(time_in_days(0,48,4))
+days = years * 365 + weeks * 7 + days * 1
+print(amount_of_days(0,48,4))
